d24a32b9da94f4babf56df3b359f91142006bdc6
dolah <david.olah@cofinpro.de>
2017-05-30 16:11:20 +0200

    Cart Tab

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -1,88 +1,94 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Notifications } from 'expo';
+import {StyleSheet, View} from 'react-native';
+import {Notifications} from 'expo';
 import {
-  StackNavigation,
-  TabNavigation,
-  TabNavigationItem,
+    StackNavigation,
+    TabNavigation,
+    TabNavigationItem,
 } from '@expo/ex-navigation';
-import { FontAwesome } from '@expo/vector-icons';
+import {FontAwesome} from '@expo/vector-icons';
 
 import Alerts from '../constants/Alerts';
 import Colors from '../constants/Colors';
 import registerForPushNotificationsAsync
-  from '../api/registerForPushNotificationsAsync';
+    from '../api/registerForPushNotificationsAsync';
 
 export default class RootNavigation extends React.Component {
-  componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications();
-  }
+    componentDidMount() {
+        this._notificationSubscription = this._registerForPushNotifications();
+    }
 
-  componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove();
-  }
+    componentWillUnmount() {
+        this._notificationSubscription && this._notificationSubscription.remove();
+    }
 
-  render() {
-    return (
-      <TabNavigation tabBarHeight={56} initialTab="home">
-        <TabNavigationItem
-          id="home"
-          renderIcon={isSelected => this._renderIcon('home', isSelected)}>
-          <StackNavigation initialRoute="home" />
-        </TabNavigationItem>
+    render() {
+        return (
+            <TabNavigation tabBarHeight={56} initialTab="home">
+                <TabNavigationItem
+                    id="home"
+                    renderIcon={isSelected => this._renderIcon('home', isSelected)}>
+                    <StackNavigation initialRoute="home"/>
+                </TabNavigationItem>
 
-        <TabNavigationItem
-          id="links"
-          renderIcon={isSelected => this._renderIcon('book', isSelected)}>
-          <StackNavigation initialRoute="links" />
-        </TabNavigationItem>
+                <TabNavigationItem
+                    id="links"
+                    renderIcon={isSelected => this._renderIcon('book', isSelected)}>
+                    <StackNavigation initialRoute="links"/>
+                </TabNavigationItem>
 
-        <TabNavigationItem
-          id="settings"
-          renderIcon={isSelected => this._renderIcon('cog', isSelected)}>
-          <StackNavigation initialRoute="settings" />
-        </TabNavigationItem>
-      </TabNavigation>
-    );
-  }
+                <TabNavigationItem
+                    id="settings"
+                    renderIcon={isSelected => this._renderIcon('cog', isSelected)}>
+                    <StackNavigation initialRoute="settings"/>
+                </TabNavigationItem>
 
-  _renderIcon(name, isSelected) {
-    return (
-      <FontAwesome
-        name={name}
-        size={32}
-        color={isSelected ? Colors.tabIconSelected : Colors.tabIconDefault}
-      />
-    );
-  }
+                <TabNavigationItem
+                    id="cart"
+                    renderIcon={isSelected => this._renderIcon('cart-plus', isSelected)}>
+                    <StackNavigation initialRoute="cart"/>
+                </TabNavigationItem>
+            </TabNavigation>
+        );
+    }
 
-  _registerForPushNotifications() {
-    // Send our push token over to our backend so we can receive notifications
-    // You can comment the following line out if you want to stop receiving
-    // a notification every time you open the app. Check out the source
-    // for this function in api/registerForPushNotificationsAsync.js
-    registerForPushNotificationsAsync();
+    _renderIcon(name, isSelected) {
+        return (
+            <FontAwesome
+                name={name}
+                size={32}
+                color={isSelected ? Colors.tabIconSelected : Colors.tabIconDefault}
+            />
+        );
+    }
 
-    // Watch for incoming notifications
-    this._notificationSubscription = Notifications.addListener(
-      this._handleNotification
-    );
-  }
+    _registerForPushNotifications() {
+        // Send our push token over to our backend so we can receive notifications
+        // You can comment the following line out if you want to stop receiving
+        // a notification every time you open the app. Check out the source
+        // for this function in api/registerForPushNotificationsAsync.js
+        registerForPushNotificationsAsync();
 
-  _handleNotification = ({ origin, data }) => {
-    this.props.navigator.showLocalAlert(
-      `Push notification ${origin} with data: ${JSON.stringify(data)}`,
-      Alerts.notice
-    );
-  };
+        // Watch for incoming notifications
+        this._notificationSubscription = Notifications.addListener(
+            this._handleNotification
+        );
+    }
+
+    _handleNotification = ({origin, data}) => {
+        this.props.navigator.showLocalAlert(
+            `Push notification ${origin} with data: ${JSON.stringify(data)}`,
+            Alerts.notice
+        );
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  selectedTab: {
-    color: Colors.tabIconSelected,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    selectedTab: {
+        color: Colors.tabIconSelected,
+    },
 });
--- navigation/Router.js
+++ navigation/Router.js
@@ -1,14 +1,14 @@
-import { createRouter } from '@expo/ex-navigation';
-
-import HomeScreen from '../screens/HomeScreen';
-import Scan from '../screens/Scan'
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-import RootNavigation from './RootNavigation';
+import {createRouter} from "@expo/ex-navigation";
+import Scan from "../screens/Scan";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import RootNavigation from "./RootNavigation";
+import CartScreen from "../screens/CartScreen";
 
 export default createRouter(() => ({
   home: () => Scan,
   links: () => LinksScreen,
   settings: () => SettingsScreen,
   rootNavigation: () => RootNavigation,
+  cart: () => CartScreen
 }));
