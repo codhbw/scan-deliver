43f34bc32c4b1583c982a2b4fcedc39bcce9fcad
Daniel Betsche <dbetsche@gmail.com>
2017-05-31 11:01:46 +0200

    warenkorb auf flatlist geändert

--- components/ShoppingCartList.js
+++ components/ShoppingCartList.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react'
-import { SectionList, Text, StyleSheet } from 'react-native'
+import { FlatList, Text, StyleSheet } from 'react-native'
 import ShoppingCartItem from './ShoppingCartItem'
 
-const sections = [
-    {
-        id: 0,
-        key: 0,
-        title: 'Einkaufswagen',
-        data: [
-            {id: 0, name: 'T-Shirt', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-            {id: 1, name: 'Jeans', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-            {id: 2, name: 'Stoffhose', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-        ]
-    },
-    {
-        id: 1,
-        key: 1,
-        title: 'Merkliste',
-        data: [
-            {id: 3, name: 'Bluse', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-            {id: 4, name: 'Shirt', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-            {id: 5, name: 'Kurze Hose', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-        ]
-    }
+const rows = [
+    {id: 0, name: 'T-Shirt', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 1, name: 'Jeans', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 2, name: 'Stoffhose', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 3, name: 'Bluse', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 4, name: 'Shirt', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 5, name: 'Kurze Hose', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
 ]
 
 const extractKey = ({id}) => id
@@ -35,21 +21,12 @@ export default class App extends Component {
         )
     }
 
-    renderSectionHeader = ({section}) => {
-        return (
-            <Text style={styles.header}>
-                {section.title}
-            </Text>
-        )
-    }
-
     render() {
         return (
-            <SectionList
+            <FlatList
                 style={styles.container}
-                sections={sections}
+                data={rows}
                 renderItem={this.renderItem}
-                renderSectionHeader={this.renderSectionHeader}
                 keyExtractor={extractKey}
             />
         );
