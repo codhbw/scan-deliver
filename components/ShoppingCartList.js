e77202f56f1ee61e24a41a6c033a37b393a1a7fd
Daniel Betsche <dbetsche@gmail.com>
2017-05-31 14:02:44 +0200

    clothing items now have changeable sizes

--- components/ShoppingCartList.js
+++ components/ShoppingCartList.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react'
-import { FlatList, Text, StyleSheet } from 'react-native'
+import { FlatList, Text, StyleSheet, AsyncStorage } from 'react-native'
 import ShoppingCartItem from './ShoppingCartItem'
 
 const rows = [
-    {id: 0, name: 'T-Shirt', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-    {id: 1, name: 'Jeans', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-    {id: 2, name: 'Stoffhose', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-    {id: 3, name: 'Bluse', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-    {id: 4, name: 'Shirt', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
-    {id: 5, name: 'Kurze Hose', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 0, type: 'clothing', name: 'T-Shirt', size: 'm', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 1, type: 'clothing', name: 'Jeans', size: 'm', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 2, type: 'clothing', name: 'Stoffhose', size: 'm', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 3, type: 'clothing', name: 'Bluse', size: 'm', preis: '59,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 4, type: 'clothing', name: 'Shirt', size: 'm', preis: '34,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
+    {id: 5, type: 'clothing', name: 'Kurze Hose', size: 'm', preis: '89,99 €', bildUrl: 'https://www.jazzyshirt.de/content/pics/produkte/maenner/basic-t-shirt-zoom.jpg'},
 ]
+
+async function saveItems(allitems){
+    await AsyncStorage.setItem('@store:items', JSON.stringify(allitems));
+}
+
+async function loadItems(){
+    const items = await AsyncStorage.getItem('@store:items');
+    return items;
+}
+
+try {
+    saveItems(rows);
+} catch (error) {
+    // Error saving data
+}
+
+try {
+    const items = loadItems();
+    if (items !== null){
+        // We have data!!
+        console.log('success');
+        console.log(items)
+    }
+} catch (error) {
+    // Error retrieving data
+}
 
 const extractKey = ({id}) => id
 
 export default class App extends Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     renderItem = ({item}) => {
         return (
-            <ShoppingCartItem name={item.name} preis={item.preis} bildUrl={item.bildUrl}/>
+            <ShoppingCartItem type={item.type} name={item.name} size={item.size} preis={item.preis} bildUrl={item.bildUrl}/>
         )
     }
 
